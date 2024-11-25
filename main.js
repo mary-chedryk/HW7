8729c6d37c89dfb905d3acdc41a3cb5009054cd6
Mariia Chedryk <70023995+mary-chedryk@users.noreply.github.com>
2024-11-25 16:24:30 +0200

    corections

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ numbers[1] = 10;
 
 console.log(numbers);
 
+
 //Task 2
 const rows = [
-    ["Row 1"],
-    ["Row 2"],
-    ["Row 3"], 
+    "Row 1",
+    "Row 2",
+    "Row 3", 
 ]
 
 rows.push("Row 4");
@@ -16,17 +17,21 @@ rows.push("Row 4");
 console.log(rows);
 
 //Task 3
- let sum = 6;
- let sum2 = 5;
- let sum3 = 3;
- console.log(sum + sum2 + sum3);
-
- //Task 4
+ function sumArray(arr) {
+    return arr.reduce((acc, num) => acc + num, 0);
+  }
+  const sum = [1, 2, 3, 4, 5];
+  const totalSum = sumArray(sum);
+  console.log(totalSum); 
+  
+ 
+  //Task 4
 let separate = [10, 20, 30, 40, 50];
 
 for (let i = 0; i < separate.length; i++) {
     console.log(separate[i]);
 }
+
 
 //Task 5
 let strings = ["C++", "Java", "JavaScript", "Puthon", "PHP"];
@@ -37,12 +42,14 @@ for (let i = 0; i < strings.length; i++) {
     }
 }
 
+
 //Task 6
 let max = [12, 34, 7, 56, 89, 2, 45, 78, 99, 33];
 
 let maxNumber = Math.max(...max);
 
 console.log("Максимальне значення в масиві:", maxNumber);
+
 
 //Task 7
 let even = [12, 34, 7, 56, 89, 2, 45, 78, 99, 33];
